746c163af75ec3ded138f20ce86de297f0b9f4ae
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-06-21 16:49:45 +0330

    Q2: average | using argparse module

---
+++ Week11/Q2-average-argparse.py
@@ -0,0 +1,13 @@
+import argparse
+
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('-g', '--grades', help='your grades', type=float, nargs='+')
+parser.add_argument('-f', '--float', help='number of decimal places', type=int, default=2)
+
+args = parser.parse_args()
+average = sum(args.grades) / len(args.grades)
+print(f'average: {average:.{args.float}f}')
+
+
