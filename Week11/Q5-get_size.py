7b71f708c3d991086139a21f99c0fec5205281b1
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-06-21 20:33:45 +0330

    Q2: get size | get total size of a path

---
+++ Week11/Q5-get_size.py
@@ -0,0 +1,54 @@
+import argparse
+import pathlib
+import os
+
+
+def get_path_size(input_path, format=None):
+    total_size = 0
+    if os.path.isfile(input_path):
+        total_size += os.path.getsize(input_path)
+
+    elif os.path.isdir(input_path):
+        if format:
+            for item in os.listdir(input_path):
+                if item.endswith(format):
+                    new_path = os.path.join(input_path, item)
+                    total_size += get_path_size(new_path)
+
+        else:
+            for item in os.listdir(input_path):
+                new_path = os.path.join(input_path, item)
+                total_size += get_path_size(new_path)
+        
+    return total_size
+
+
+def convert_B_to_KB(size):
+    return f'size: {size/1024:.3f} KB'
+
+
+parser = argparse.ArgumentParser()
+group = parser.add_mutually_exclusive_group()
+group.add_argument('-d', type=pathlib.Path)
+group.add_argument('-f', type=argparse.FileType())
+parser.add_argument('-F', type=str)
+args = parser.parse_args()
+
+if args.d:
+    if args.F:
+        print(convert_B_to_KB(get_path_size(args.d, format=args.F)))
+    else:
+        print(convert_B_to_KB(get_path_size(args.d)))
+
+elif args.f:
+    print(convert_B_to_KB(get_path_size(args.f)))
+
+
+
+
+
+# path for test in my laptop:
+# & "C:/Program Files/python.exe" Q5-get_size.py -d E:\Python\Maktab_Sharif\W11\HW\HW-98\Week11
+# & "C:/Program Files/python.exe" Q5-get_size.py -d E:\Python\Maktab_Sharif\W11\HW\HW-98\Week11 -F py
+# & "C:/Program Files/python.exe" Q5-get_size.py -d E:\Python\Maktab_Sharif\W11\HW\HW-98\Week11\Q5-get_size.py
+# & "C:/Program Files/python.exe" Q5-get_size.py -d E:\Python\Maktab_Sharif\W11\HW\HW-98\Week11\Q5-get_size.py -F py
