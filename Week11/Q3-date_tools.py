e17503a9739703be2de5cbb207f1937274461785
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-06-21 22:18:33 +0330

    Q3: date tools | writing some tools for date and time

---
+++ Week11/Q3-date_tools.py
@@ -0,0 +1,50 @@
+import datetime
+import calendar
+import jdatetime
+
+
+class Date_tools:
+    def __init__(self, first_date, second_date):
+        self.first_date = first_date
+        self.second_date = second_date
+
+
+    def time_difference_in_seconds(self):
+        time_difference = (self.second_date - self.first_date).total_seconds()
+        print(f"time difference in seconds: {time_difference}")
+
+
+    def number_of_leap_years(self):
+        count = 0
+        for year in range(self.first_date, self.second_date + 1):
+            if calendar.isleap(year):
+                count += 1
+        print(f"number of leap years: {count}")
+
+
+    def number_of_daylight_saving(self):
+        pass
+
+
+    def convert_to_hijri(self):
+        j_first_date = jdatetime.datetime.fromgregorian(self.first_date)
+        j_second_date = jdatetime.datetime.fromgregorian(self.second_date)
+        print(f"first date in Hijri: {j_first_date}")
+        print(f"second date in Hijri: {j_second_date}")
+
+
+
+def main():
+    first_date = input("Enter first date and time in this format: 'year-month-day Hour:Minute:Second': ")
+    second_date = input("Enter second date and time in this format: 'year-month-day Hour:Minute:Second': ")
+
+    first_date = datetime.datetime.strptime(first_date, '%Y-%m-%d %H:%M:%S')
+    second_date = datetime.datetime.strptime(second_date, '%Y-%m-%d %H:%M:%S')
+
+    two_times = Date_tools(first_date, second_date)
+    two_times.time_difference_in_seconds()
+    two_times.number_of_leap_years()
+    two_times.convert_to_hijri
+
+if __name__ == "__main__":
+    main()
