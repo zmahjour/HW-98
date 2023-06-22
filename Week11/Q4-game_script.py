607fc3bdb543191adbc8c8f8779eb010d19608b7
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-06-22 08:14:58 +0330

    Q4: game script | guess number game

--- Week11/Q4-game_script.py
+++ Week11/Q4-game_script.py
@@ -10,10 +10,10 @@ parser.add_argument('-g', type=int, default=5)
 
 args = parser.parse_args()
 
-correct_num = random.randint(0, 100)
+correct_num = random.randint(args.s, args.e)
 guess = None
 
-for i in range(5):
+for i in range(args.g):
     guess = int(input("What is your guess? "))
 
     if guess < correct_num:
