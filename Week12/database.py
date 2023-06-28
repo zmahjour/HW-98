9f45703fe5472d2a7f5c35ebc94516370559f7f8
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-06-28 17:40:39 +0330

    Weather Server Project | database.py | writing get_request_id method and some other edits

--- Week12/database.py
+++ Week12/database.py
@@ -4,7 +4,7 @@ from typing import List, Tuple, Any
 
 class WeatherDatabase:
     @staticmethod
-    def connect_to_db() -> Any:
+    def connect_to_db():
         conn = psycopg2.connect(
             database="weather", user="postgres", password="13751375", port=5433
         )
@@ -12,30 +12,30 @@ class WeatherDatabase:
 
     @classmethod
     def create_request_table(cls) -> None:
-        conn = WeatherDatabase.connect_to_db()
+        conn = cls.connect_to_db()
         with conn.cursor() as cur:
             cur.execute(
                 """
                 CREATE TABLE IF NOT EXISTS request (
                 request_id SERIAL PRIMARY KEY,
                 city_name VARCHAR(50),
-                request_time TIMESTAMP
+                request_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP
                 )
             """
             )
             conn.commit()
-            conn.close()
+        conn.close()
 
     @classmethod
     def create_response_table(cls) -> None:
-        conn = WeatherDatabase.connect_to_db()
+        conn = cls.connect_to_db()
         with conn.cursor() as cur:
             cur.execute(
                 """
                 CREATE TABLE IF NOT EXISTS response (
                 response_id SERIAL PRIMARY KEY,
-                request_id REFERENCES request (request_id),
-                response_time TIMESTAMP,
+                request_id INTEGER REFERENCES request (request_id) UNIQUE NOT NULL,
+                response_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
                 status_code INTEGER,
                 temperature REAL,
                 feels_like REAL,
@@ -44,63 +44,81 @@ class WeatherDatabase:
             """
             )
             conn.commit()
-            conn.close()
+        conn.close()
 
     @classmethod
-    def save_request_data(cls, city_name: str, request_time: str) -> None:
-        conn = WeatherDatabase.connect_to_db()
+    def save_request_data(cls, city_name: str) -> None:
+        conn = cls.connect_to_db()
+        with conn.cursor() as cur:
+            cur.execute(f"INSERT INTO request (city_name) VALUES ('{city_name}')")
+            conn.commit()
+        conn.close()
+
+    @classmethod
+    def save_response_data(
+        cls, request_id, status_code, temperature, feels_like, last_updated
+    ) -> None:
+        values = (request_id, status_code, temperature, feels_like, last_updated)
+        conn = cls.connect_to_db()
         with conn.cursor() as cur:
             cur.execute(
-                f"INSERT INTO request (city_name, request_time) VALUES ({city_name}, {request_time})"
+                "INSERT INTO response (request_id, status_code, temperature, feels_like, last_updated) VALUES (%s, %s, %s, %s, %s)",
+                values,
             )
             conn.commit()
-            conn.close()
+        conn.close()
 
-    def save_response_data(self, city_name: str, response_data: dict) -> None:
-        """
-        Save response data for a city to the database.
+    @classmethod
+    def get_request_id(cls) -> int:
+        conn = cls.connect_to_db()
+        with conn.cursor() as cur:
+            cur.execute(
+                f"SELECT request_id FROM request ORDER BY request_id DESC LIMIT 1"
+            )
+            row = cur.fetchall()
+        conn.close()
+        return row[0]
 
-        Args:
-        - city_name (str): The name of the city to save response data for.
-        - response_data (dict): A dictionary containing weather information for the city, including temperature, feels like temperature, and last updated time.
+    @classmethod
+    def get_request_count(cls) -> int:
+        conn = cls.connect_to_db()
+        with conn.cursor() as cur:
+            cur.execute(f"SELECT COUNT(*) FROM request")
+            row = cur.fetchall()
+        conn.close()
+        return row[0]
 
-        Returns:
-        - None
-        """
-        pass
+    @classmethod
+    def get_successful_request_count(cls) -> int:
+        conn = cls.connect_to_db()
+        with conn.cursor() as cur:
+            cur.execute(f"SELECT COUNT(*) FROM response WHERE status_code = 200")
+            row = cur.fetchall()
+        conn.close()
+        return row[0]
 
-    def get_request_count(self) -> int:
-        """
-        Get the total number of requests made to the server.
+    @classmethod
+    def get_last_hour_requests(cls) -> List[Tuple[str, str]]:
+        conn = cls.connect_to_db()
+        with conn.cursor() as cur:
+            cur.execute(
+                f"SELECT city_name, request_time FROM request WHERE request_time >= CURRENT_TIMESTAMP - INTERVAL '1 hour'"
+            )
+            rows = cur.fetchall()
+        conn.close()
+        return rows
 
-        Returns:
-        - int: The total number of requests made to the server.
-        """
-        pass
+    @classmethod
+    def get_city_request_count(cls) -> List[Tuple[str, int]]:
+        conn = cls.connect_to_db()
+        with conn.cursor() as cur:
+            cur.execute(
+                f"SELECT city_name, COUNT(*) AS number_of_requests FROM request GROUP BY city_name"
+            )
+            rows = cur.fetchall()
+        conn.close()
+        return rows
 
-    def get_successful_request_count(self) -> int:
-        """
-        Get the total number of successful requests made to the server.
 
-        Returns:
-        - int: The total number of successful requests made to the server.
-        """
-        pass
-
-    def get_last_hour_requests(self) -> List[Tuple[str, str]]:
-        """
-        Get a list of requests made in the last hour.
-
-        Returns:
-        - List[Tuple[str, str]]: A list of tuples containing the name of the city and the time the request was made, in ISO format.
-        """
-        pass
-
-    def get_city_request_count(self) -> List[Tuple[str, int]]:
-        """
-        Get a count of requests made for each city.
-
-        Returns:
-        - List[Tuple[str, int]]: A list of tuples containing the name of the city and the number of requests made for that city.
-        """
-        pass
+WeatherDatabase.create_request_table()
+WeatherDatabase.create_response_table()
