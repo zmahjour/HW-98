49aee08d507c18775f819c5741072988b623f0cf
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-07-19 20:56:27 +0330

    TaskManagementProject | editing urls.py to send pk in the url

--- Week15/TaskManagementProject/task/urls.py
+++ Week15/TaskManagementProject/task/urls.py
@@ -4,6 +4,6 @@ from task import views
 urlpatterns = [
     path("home/", views.home, name="home"),
     path("search/", views.search, name="search"),
-    path("detail/<int:id>/", views.task_detail, name="task_detail"),
+    path("detail/<int:pk>/", views.task_detail, name="task_detail"),
     path("all/", views.all_tasks, name="all_tasks"),
 ]
