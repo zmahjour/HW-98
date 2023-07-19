508090c3fe67a6d757ee1f8308ac4db03ae233e2
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-07-19 15:56:35 +0330

    TaskManagementProject | editing urls.py

--- Week15/TaskManagementProject/task/urls.py
+++ Week15/TaskManagementProject/task/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from task import views
 
 urlpatterns = [
-    path("home/", views.home, neme="home"),
+    path("home/", views.home, name="home"),
     path("search/", views.search, name="search"),
     path("detail/<int:id>/", views.task_detail, name="task_detail"),
     path("all/", views.all_tasks, name="all_tasks"),
