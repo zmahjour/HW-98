4357c97a5a5140e7e6a02026a3568a173799f8e1
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-07-20 02:56:41 +0330

    TaskManagementProject | registering models

--- Week15/TaskManagementProject/task/admin.py
+++ Week15/TaskManagementProject/task/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from task.models import Task
+from task.models import Task, Tag, Category
 
 # Register your models here.
 admin.site.register(Task)
+admin.site.register(Tag)
+admin.site.register(Category)
