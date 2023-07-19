a09ea91315d964abb1c36ab64b47d97c4ceab051
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-07-19 13:20:55 +0330

    TaskManagementProject | creating the base of the views

--- Week15/TaskManagementProject/task/views.py
+++ Week15/TaskManagementProject/task/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    return render(request, "home.html", context=tasks)
+
+
+def search(request):
+    pass
+
+
+def task_detail(request):
+    return render(request, "task_detail.html", context=task)
+
+
+def all_tasks(request):
+    return render(request, "all_tasks.html", context=tasks)
