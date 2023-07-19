58bd5d890631eeb58817fd05fc4dc4870803e48c
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-07-19 11:20:06 +0330

    TaskManagementProject | creating task app and its models

---
+++ Week15/TaskManagementProject/task/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField(blank=True)
+
+
+class Tag(models.Model):
+    label = models.CharField(max_length=20)
+
+
+class Task(models.Model):
+    NOT_STARTED = "Not started"
+    IN_PROGRESS = "In progress"
+    COMPLETED = "Completed"
+    STATUS_CHOICES = [
+        (NOT_STARTED, "Not started"),
+        (IN_PROGRESS, "In progress"),
+        (COMPLETED, "Completed"),
+    ]
+    title = models.CharField(max_length=50)
+    description = models.TextField(blank=True)
+    due_date = models.DateField(blank=True)
+    status = models.CharField(
+        max_length=20, choices=STATUS_CHOICES, default=NOT_STARTED
+    )
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank=True)
+    tags = models.ManyToManyField(Tag)
