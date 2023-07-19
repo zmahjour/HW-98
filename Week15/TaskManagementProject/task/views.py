646b6f464ac5d5afd3e2cf6efdcb3fdb4dfbcc96
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-07-20 03:01:26 +0330

    TaskManagementProject | editing views.py

--- Week15/TaskManagementProject/task/views.py
+++ Week15/TaskManagementProject/task/views.py
@@ -1,28 +1,28 @@
 from django.shortcuts import render
+from django.db.models import Q
 from task.models import Task
 
 
 def home(request):
-    tasks = Task.objects.all().order_by("-id")[0:5]
-    return render(request, "home.html", context={"tasks": tasks})
+    tasks = Task.objects.all().order_by("-id")[0:4]
+    return render(request, "home.html", {"tasks": tasks})
 
 
 def search(request):
     if request.method == "GET":
         search = request.GET["search"]
         results = Task.objects.filter(
-            title__contains=search, tags__label__contains=search
-        )
-    return render(
-        request, "search.html", context={"search": search, "results": results}
-    )
+            Q(title__icontains=search) | Q(tags__label__icontains=search)
+        ).distinct()
+
+        return render(request, "search.html", {"search": search, "results": results})
 
 
-def task_detail(request, pk):
-    task = Task.objects.get(pk=pk)
-    return render(request, "task_detail.html", context={"task": task})
+def task_detail(request, task_id):
+    task = Task.objects.get(pk=task_id)
+    return render(request, "task_detail.html", {"task": task})
 
 
 def all_tasks(request):
     tasks = Task.objects.all().order_by("-id")
-    return render(request, "all_tasks.html", context={"tasks": tasks})
+    return render(request, "all_tasks.html", {"tasks": tasks})
