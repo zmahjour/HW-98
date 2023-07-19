d6fb293bb802e92e571fd225bea0055edcfd1afb
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-07-19 20:54:58 +0330

    TaskManagementProject | writing view functions

--- Week15/TaskManagementProject/task/views.py
+++ Week15/TaskManagementProject/task/views.py
@@ -4,16 +4,25 @@ from task.models import Task
 
 def home(request):
     tasks = Task.objects.all().order_by("-id")[0:5]
-    return render(request, "home.html", {"tasks": tasks})
+    return render(request, "home.html", context={"tasks": tasks})
 
 
 def search(request):
-    pass
+    if request.method == "GET":
+        search = request.GET["search"]
+        results = Task.objects.filter(
+            title__contains=search, tags__label__contains=search
+        )
+    return render(
+        request, "search.html", context={"search": search, "results": results}
+    )
 
 
-def task_detail(request):
-    return render(request, "task_detail.html", context=task)
+def task_detail(request, pk):
+    task = Task.objects.get(pk=pk)
+    return render(request, "task_detail.html", context={"task": task})
 
 
 def all_tasks(request):
-    return render(request, "all_tasks.html", context=tasks)
+    tasks = Task.objects.all().order_by("-id")
+    return render(request, "all_tasks.html", context={"tasks": tasks})
